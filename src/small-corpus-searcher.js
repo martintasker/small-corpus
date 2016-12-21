1a308de615f20246b885d360d03996a60c3e2e9f
Martin Tasker <martin.tasker@databatix.com>
2016-12-21 06:13:43 +0000

    initial code grab from client project

---
+++ src/small-corpus-searcher.js
@@ -0,0 +1,81 @@
+class SmallCorpusSearcher {
+  constructor(docs, getString) {
+    var self = this;
+
+    this.docs = [];
+    this.IDF = {};
+
+    this.docs = docs.map(function(doc) {
+      return {
+        doc: doc,
+        words: self._wordsFromString(getString(doc))
+      };
+    });
+
+    var corpusWordIndex = {}; // count of documents in corpus including each word
+
+    this.docs.forEach(function(doc) {
+      var docWordIndex = {};
+      doc.words.forEach(function(word) {
+        docWordIndex[word] = true;
+      });
+      Object.keys(docWordIndex).forEach(function(word) {
+        corpusWordIndex[word] = corpusWordIndex[word] + 1 || 1;
+      });
+    });
+
+    Object.keys(corpusWordIndex).forEach(function(word) {
+      self.IDF[word] = 1 / (1 + Math.log(corpusWordIndex[word]));
+    });
+
+    corpusWordIndex = null; // free the memory
+  }
+
+  _wordsFromString(string) {
+    string = string.replace(/'/gi, '');
+    string = string.replace(/[^\w\s]/gi, ' ');
+    return string.toLowerCase().split(" ").filter(function(v) {
+      return !!v;
+    });
+  };
+
+  filter(term) {
+    var self = this;
+    var terms = this._wordsFromString(term);
+    var hits = this.docs.map(function(item) {
+      var missedAnyTerm = false;
+      var bestTermRank = 0;
+      terms.forEach(function(term) {
+        var hitThisTerm = false;
+        var thisTermRank = 0;
+        item.words.forEach(function(word) {
+          if (word.indexOf(term) === 0) {
+            thisTermRank = Math.max(thisTermRank, self.IDF[word]);
+            hitThisTerm = true;
+          }
+        });
+        if (!hitThisTerm) {
+          missedAnyTerm = true;
+        } else {
+          bestTermRank = Math.max(bestTermRank, thisTermRank);
+        }
+      });
+      var res = {
+        item: item,
+        rank: missedAnyTerm
+          ? 0
+          : bestTermRank
+      };
+      return res;
+    }).filter(function(item) {
+      return !term || item.rank > 0;
+    }).sort(function(a, b) {
+      return b.rank - a.rank;
+    }).map(function(item) {
+      return item.item.doc;
+    });
+    return hits;
+  }
+}
+
+export default SmallCorpusSearcher;
---
+++ src/small-corpus-searcher.spec.js
@@ -0,0 +1,77 @@
+import SmallCorpusSearcher from './small-corpus-searcher';
+
+describe('SmallCorpusSearcher', function() {
+
+  // test data
+  var DOCS = [
+    'the quick brown fox jumps over the lazy dog',
+    'the rain in Spain falls mainly on the plain',
+    'to be, or not to be, that is the question'
+  ];
+
+  function getWords(doc) {
+    return doc;
+  }
+
+  // test instance
+  var that;
+
+  beforeEach(function() {
+    that = new SmallCorpusSearcher(DOCS, getWords);
+  });
+
+  describe('initialization', function() {
+    it('has public interface', function() {
+      expect(typeof that._wordsFromString).toEqual('function');
+      expect(typeof that.filter).toEqual('function');
+    });
+    it('has properties', function() {
+      expect(typeof that.docs).toEqual('object');
+      expect(typeof that.IDF).toEqual('object');
+      expect(Object.keys(that).length).toEqual(2);
+    });
+    it('converts all documents', function() {
+      expect(that.docs.length).toEqual(3);
+    });
+    it('converts to { doc:, words: } structure', function() {
+      for (var i = 0; i < that.docs.length; i++) {
+        expect(Object.keys(that.docs[i])).toEqual(['doc', 'words']);
+      }
+    });
+    it('splits words correctly', function() {
+      expect(that.docs[0].words).toEqual(['the', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']);
+    });
+    it('gets IDF of "the" and "fox" in right order', function() {
+      expect(that.IDF['the']).toBeLessThan(that.IDF['fox']);
+    });
+    it('gets IDF of "spain" and "fox" equal', function() {
+      expect(that.IDF['spain']).toEqual(that.IDF['fox']);
+    });
+    it('gets IDF of "to" and "fox" equal', function() {
+      expect(that.IDF['to']).toEqual(that.IDF['fox']);
+    });
+  });
+
+  describe('_wordsFromString()', function() {
+    it('splits nicely', function() {
+      var res = that._wordsFromString('the quick brown fox');
+      expect(res).toEqual(['the', 'quick', 'brown', 'fox']);
+    });
+    it('lower-cases things', function() {
+      var res = that._wordsFromString('the Quick Brown Fox');
+      expect(res).toEqual(['the', 'quick', 'brown', 'fox']);
+    });
+    it('gets rid of punctuation', function() {
+      var res = that._wordsFromString('"the quick, brown, fox!"');
+      expect(res).toEqual(['the', 'quick', 'brown', 'fox']);
+    });
+    it('ignores apostrophes without word-breaking', function() {
+      var res = that._wordsFromString('it\'s got its towel');
+      expect(res).toEqual(['its', 'got', 'its', 'towel']);
+    });
+    it('preserves numbers', function() {
+      var res = that._wordsFromString('How do you do, Thing1 and Thing2?');
+      expect(res).toEqual(['how', 'do', 'you', 'do', 'thing1', 'and', 'thing2']);
+    });
+  });
+});
